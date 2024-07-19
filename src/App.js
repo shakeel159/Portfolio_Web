0695ca8ff107412dc229c5f6a5aca3ad73bc9b15
shakeel159 <jazibshaq@gmail.com>
2024-07-19 10:56:06 -0500

    TwoVideoUploads

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 //components
+import Cards from './Components/Cards';
 import Aboutme from './Components/AboutMe';
+import Footer from './Components/Footer';
+//imgs
+import WebProject from '../src/Images/web.JPG';
+import addvonImg from '../src/Images/AddvonImg.PNG';
+import CrocBashImg from '../src/Images/CB_Icon.png';
+import TCWPPImg from '../src/Images/TCWPP.png';
+import ShaqProfilePic from '../src/Images/shaq.jpg';
+//videos
+import WebDemoVideo from './Videos/WebDemo.mp4';
 
 function App() {
+  const imageUrl =ShaqProfilePic;
   return (
-    <>
-      <div className="Page">
-        <div className="NameBox">
-          <h1>Jazib Shakeel</h1>
-        </div>
-        <Aboutme></Aboutme>
+  <>
+  <div className="Page">
+  <div className="NameBox">
+      <h1>Jazib Shakeel</h1>
+    </div>
+    <hr></hr>
+    <Aboutme></Aboutme>
+    <div className="Projects">
+      <h1 className='banners'>Projects</h1>
+      <div className="Cards">
+      <Cards title="RecipeSearch Website" img={WebProject} videoSrc={WebDemoVideo}></Cards>
+      <Cards title="CrocBash" img={CrocBashImg} videoSrc="https://www.youtube.com/embed/ImPOPqhTMcY"/>
       </div>
-    </>
+    </div>
+    <hr></hr>
+    <div>
+      <Footer/>
+    </div>
+
+  </div>
+  </>
+    
   );
 }
 
